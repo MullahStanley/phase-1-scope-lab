17102540bf4bbebf68f0f6e59557b1fc03fc2be9
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-03-16 22:39:32 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName= bob;
+function upperCaseCustomerName(customerName){
+    return (customerName.toUpperCase());
+}
+function setBestCustomer(bestCustomer){
+    return bestCustomer;
+}
+function overwriteBestCustomer(bestCustomer){
+    return bestCustomer= not bob;
+}
+const leastFavoriteCustomer=Jane;
+function changeLeastFavoriteCustomer(){
+    let leastFavoriteCustomer=John;
+}
